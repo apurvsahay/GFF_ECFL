4a42f99e93f255c61ba5eb377441176a40944388
Apurv <apurvasahay423@gmail.com>
2021-05-22 00:53:06 +0530

    Creating toolbar

--- app/src/main/java/com/example/gffecfl/HomeActivity.java
+++ app/src/main/java/com/example/gffecfl/HomeActivity.java
@@ -2,13 +2,46 @@ package com.example.gffecfl;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    public void showPopup(View view) {
+        PopupMenu popup = new PopupMenu(this,view);
+        popup.setOnMenuItemClickListener(this);
+        popup.inflate(R.menu.options);
+        popup.show();
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.option1:
+                Intent intent = new Intent(HomeActivity.this,AdminActivity.class);
+                HomeActivity.this.startActivity(intent);
+                return true;
+            case R.id.option2:
+                Intent intent1 = new Intent(HomeActivity.this,MainActivity.class);
+                HomeActivity.this.startActivity(intent1);
+                return true;
+        }
+        return false;
     }
 }
--- app/src/main/java/com/example/gffecfl/MainActivity.java
+++ app/src/main/java/com/example/gffecfl/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.gffecfl;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
