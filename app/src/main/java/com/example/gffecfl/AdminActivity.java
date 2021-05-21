01ef76b8d77ee7a381279cf689d332ceb1726f02
Apurv <apurvasahay423@gmail.com>
2021-05-22 04:06:04 +0530

    Admin Panel

--- app/src/main/java/com/example/gffecfl/AdminActivity.java
+++ app/src/main/java/com/example/gffecfl/AdminActivity.java
@@ -2,10 +2,15 @@ package com.example.gffecfl;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.PopupMenu;
 
-public class AdminActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class AdminActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +19,23 @@ public class AdminActivity extends AppCompatActivity {
     }
 
     public void showPopup(View view) {
+        PopupMenu popup = new PopupMenu(this,view);
+        popup.setOnMenuItemClickListener(this);
+        popup.inflate(R.menu.options_admin);
+        popup.show();
+    }
 
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.optionAddPlayer:
+                return true;
+            case R.id.optionEliminateCountry:
+                return true;
+            case R.id.optionSellPlayer:
+                return true;
+        }
+        return false;
     }
 }
