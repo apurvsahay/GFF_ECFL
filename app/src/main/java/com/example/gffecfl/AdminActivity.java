6516f67708e1382b5c50a8918084188108f51ee1
Apurv <apurvasahay423@gmail.com>
2021-05-22 03:23:22 +0530

    Admin Panel

--- app/src/main/java/com/example/gffecfl/AdminActivity.java
+++ app/src/main/java/com/example/gffecfl/AdminActivity.java
@@ -3,6 +3,7 @@ package com.example.gffecfl;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class AdminActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class AdminActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+    }
+
+    public void showPopup(View view) {
+
     }
 }
