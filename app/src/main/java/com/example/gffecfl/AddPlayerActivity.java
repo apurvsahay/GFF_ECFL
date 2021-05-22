ec08cf9c0288a26371ff55d3870946f2ec80069f
Apurv <apurvasahay423@gmail.com>
2021-05-23 01:08:23 +0530

    Player Countries

--- app/src/main/java/com/example/gffecfl/AddPlayerActivity.java
+++ app/src/main/java/com/example/gffecfl/AddPlayerActivity.java
@@ -12,72 +12,65 @@ import android.widget.Spinner;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class AddPlayerActivity extends AppCompatActivity {
 
     AutoCompleteTextView countries, playerPosition;
     List<String> pos= new ArrayList<>();
-//    String[] country = new String[24];
-//    List<String> countryList = new ArrayList<>();
+    List<String> countryList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_player);
 
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
         pos.add("Goalkeeper");
         pos.add("Defender");
         pos.add("Midfielder");
         pos.add("Forward");
 
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Countries");
-        reference.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference countryReference = reference.child("Countries");
+
+        countryReference.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onComplete(@NonNull @NotNull Task<DataSnapshot> task) {
-                if (task.isSuccessful()) {
-                    //countryList = setCountryNames((Map<String, String>) task.getResult().getValue());
-                    //country = setCountryNamesArray((Map<String, String>) task.getResult().getValue());
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    countryList.add(dataSnapshot.getValue(String.class));
                 }
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
             }
         });
 
         countries = (AutoCompleteTextView) findViewById(R.id.addPlayerCountry);
         playerPosition = (AutoCompleteTextView) findViewById(R.id.addPlayerPosition);
 
-//        ArrayAdapter<String> arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, country);
-//        countries.setThreshold(1);
-//        countries.setAdapter(arrayAdapter);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, countryList);
+        countries.setThreshold(0);
+        countries.setAdapter(arrayAdapter);
 
         ArrayAdapter<String> arrayAdapter1 = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, pos);
         playerPosition.setThreshold(0);
         playerPosition.setAdapter(arrayAdapter1);
-    }
-
-    private String[] setCountryNamesArray(Map<String, String> value) {
-        String[] array= new String[24];
-        int count=0;
-        for (Map.Entry<String,String> entry : value.entrySet()){
-            array[count] = entry.getValue();
-            count++;
-        }
-        return array;
-    }
-
-    private List<String> setCountryNames(Map<String, String> value) {
-        List<String> list = new ArrayList<>();
-        for (Map.Entry<String,String> entry : value.entrySet()){
-            String country = entry.getValue();
-            list.add(country);
-        }
-
-        return list;
     }
 }
